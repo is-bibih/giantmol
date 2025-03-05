02f2622e254dc08ff0cf6ee7c60f587578e6d9ff
vivi <vivianamaldes@gmail.com>
2025-03-05 10:55:18 +0100

    new attempt at convolution

--- distributions.py
+++ distributions.py
@@ -14,11 +14,98 @@ def maxwell_boltzmann_1d(x, a=1.0):
 def maxwell_boltzmann_3d(x, a=1.0):
     return 4 * x**2 /(a**3 * np.sqrt(np.pi)) * np.exp(-(x/a)**2) # × v²dv, from ramsey
 
+def beam(x, a=1.0):
+    return 2 * x**3 / a**4 * np.exp(-(x/a)**2) # × v³dv, from ramsey
+
 def natural_linewidth(x, gam=1.0, P0=1.0):
     return P0 * gam/(2*np.pi) / (x**2 + (gam/2)**2)
 
 def excited_population(x, sat=1.0, gam=1.0):
     return sat/2 / (1 + sat + (2*x/gam)**2)
+
+def voigt1d(om, vp=V_P, om0=2*np.pi*PEAK_FREQ, sat=1.0, tau=TAU, num_om=2**8+1, om_max=None):
+    om = om.reshape((om.size, 1))
+    k = om0/cst.speed_of_light
+    om_max = vp*k * 5
+    gam = 1/tau
+
+    # the integral is from 0 to (om_max-om0)
+    # ompr is the integration variable
+    # ompr_mb is centered around 0 so that the maxwell-boltzmann distribution is different from 0 in the integration domain
+    ompr_mb = np.linspace(-om_max, om_max, num=num_om).reshape((1, num_om)) # (ω₀-ω') for integration
+    ompr = om0 - ompr_mb # ω' for integration
+    dom = np.abs(ompr[0,1] - ompr[0,0])
+    integration_array = (1/k) * maxwell_boltzmann_1d(ompr_mb/k, vp) \
+        * excited_population(om - ompr, sat, gam)
+    integration_array = intg.trapezoid(integration_array, dx=dom, axis=-1)
+
+    return integration_array
+
+def voigt3d_common(vel_dist, om, theta, vp=V_P, om0=2*np.pi*PEAK_FREQ, sat=1.0, tau=TAU, num_om=2**8+1, om_max=None):
+    om = np.reshape(om, (np.size(om), 1))
+    theta = np.reshape(theta, (1, np.size(theta)))
+    k = om0 * np.cos(theta) / cst.speed_of_light
+    om_max = vp*k * 3
+    gam = 1/tau
+
+    # the integral is from om0 to (om_max+om0) (positive v)
+    # ompr is the integration variable
+    ompr = np.linspace(om0, om_max+om0, num=num_om).reshape((1, num_om)) # ω' for integration
+    ompr_mb = ompr - om0 # (ω₀-ω') for integration on maxwell-boltzmann
+    dom = np.abs(ompr[0,1] - ompr[0,0])
+    integration_array = np.abs(1/k) * vel_dist(ompr_mb/k, vp) \
+        * excited_population(om - ompr, sat, gam)
+    integration_array = intg.trapezoid(integration_array, dx=dom, axis=-1)
+
+    return integration_array
+
+def voigt3d_gas(om, theta, vp=V_P, om0=2*np.pi*PEAK_FREQ, sat=1.0, tau=TAU, num_om=2**8+1, om_max=None):
+    return voigt3d_common(maxwell_boltzmann_3d, om, theta, vp, om0, sat, tau, num_om, om_max)
+
+def voigt3d_beam(om, theta, vp=V_P, om0=2*np.pi*PEAK_FREQ, sat=1.0, tau=TAU, num_om=2**8+1, om_max=None):
+    return voigt3d_common(beam, om, theta, vp, om0, sat, tau, num_om, om_max)
+
+def new_signal(om, theta, vp=V_P, om0=2*np.pi*PEAK_FREQ, sat=1.0, tau=TAU, theta_OL=0.0,
+               num_om=2**8+1, om_max=None, num_theta=2**8+1, theta_min=-np.pi/2, theta_max=np.pi/2):
+    """
+    om: laser frequency (rad/s)
+    theta: angle between laser wave-number and velocity vector (rad)
+    vp: most probable speed (m/s)
+    om0: transition frequency at rest (rad/s)
+    sat: saturation parameter
+    tau: transition lifetime
+    theta_OL: angle between laser wave-number and oven most probable velocity (rad)
+    num_om: number of integration points along omega
+    om_max: number to use as infinity
+    num_theta: number of integration points along theta
+    theta_min, theta_max: integration limits for theta (as velocity coordinate)
+    """
+    # converting between velocity coordinate, and angle w.r.t. laser
+    thetav2thetal = lambda th: theta_OL - th
+
+    # constants
+    k = om0 / cst.speed_of_light
+    om_max = vp*k * 3 if not om_max else om_max
+    gam = 1/tau
+
+    # reshaped arrays
+    # integration array will have shape (om.size, theta.size, num_om, num_theta)
+    om = np.reshape(om, (np.size(om), 1, 1, 1))
+    theta = np.reshape(theta, (1, np.size(theta), 1, 1))
+
+    # integration variables
+    ompr = np.linspace(om0, om_max+om0, num=num_om).reshape((1, 1, num_om, 1)) # ω'
+    thetapr = np.linspace(theta_min, theta_max, num=num_theta).reshape((1, 1, 1, num_theta)) # θ'
+    k_proj = k * np.cos(thetav2thetal(thetapr)) # projection of k along v
+    dom = ompr[0,0,0,1] - ompr[0,0,0,0]
+    dth = thetapr[0,0,1,0] - thetapr[0,0,0,0]
+
+    integration_array = np.pi * np.sin(2*thetapr) / k_proj \
+        * excited_population(om * np.cos(theta) - ompr, sat=sat, gam=gam) \
+        * maxwell_boltzmann_3d((om0 - ompr)/(k * np.cos(thetapr)), a=vp)
+    integration_array = intg.trapezoid(integration_array, dx=dom, axis=2) # integrate along ω'
+    integration_array = intg.trapezoid(integration_array, dx=dth, axis=-1) # integrate along θ'
+    return integration_array
 
 def P_v(v, th, vp=V_P, theta_L=0.0, om0=2*np.pi*PEAK_FREQ,
         tau=TAU, num_theta=2**8+1, num_v=2**8+1, v_max=V_MAX,
@@ -61,35 +148,6 @@ def P_v(v, th, vp=V_P, theta_L=0.0, om0=2*np.pi*PEAK_FREQ,
     integration_array = integrate(integration_array, dx=dv, axis=-1) # along v
     return integration_array
 
-def gas_dist1d(om, th=0.0, vp=V_P, om0=2*np.pi*PEAK_FREQ, sat=1.0, tau=TAU, num_om=2**8+1, om_max=None):
-    om = om.reshape((om.size, 1))
-    k = om0/cst.speed_of_light
-    om_max = vp*k * 5
-    gam = 1/tau
-
-    # the integral is from 0 to (om_max-om0)
-    # ompr is the integration variable
-    # ompr_mb is centered around 0 so that the maxwell-boltzmann distribution is different from 0 in the integration domain
-    ompr_mb = np.linspace(-om_max, om_max, num=num_om).reshape((1, num_om)) # (ω₀-ω') for integration
-    ompr = om0 - ompr_mb # ω' for integration
-    dom = np.abs(ompr[0,1] - ompr[0,0])
-    integration_array = (1/k) * maxwell_boltzmann_1d(ompr_mb/k, vp) \
-        * excited_population(om - ompr, sat, gam)
-    integration_array = intg.trapezoid(integration_array, dx=dom, axis=-1)
-
-    return integration_array
-
-def gas_dist3d(v, th=0.0, vp=V_P, theta_L=0.0, om0=2*np.pi*PEAK_FREQ, tau=TAU, num_v=2**8+1, v_max=V_MAX):
-    v = v.reshape((v.size, 1))
-    alphainv = lambda th: get_alphainv(th, om0, theta_L)
-    gam = 1/tau
-
-    vpr = np.linspace(-v_max, v_max, num=num_v).reshape((1,num_v)) # v prime for integration
-    dv = vpr[0,1] - vpr[0,0]
-    integration_array = np.abs(alphainv(th)) * maxwell_boltzmann_3d(vpr, vp) * natural_linewidth(v*alphainv(th) - vpr*alphainv(th), gam)
-    integration_array = intg.trapezoid(integration_array, dx=dv, axis=-1)
-
-    return integration_array
 
 def signal_v(v, th, vp=V_P, theta_L=0.0, om0=2*np.pi*PEAK_FREQ,
              sat=1.0, tau=TAU, num_theta=2**8+1, num_v=2**8+1, v_max=V_MAX,
--- sanity_checks.py
+++ sanity_checks.py
@@ -9,92 +9,215 @@ import utilities as ut
 def dist1d():
     om0 = 2*np.pi*PEAK_FREQ
     k = om0/cst.speed_of_light
+    T = 500
+    sat = 0.1
 
     num = 501
-    vt = ut.temp2vel(300+273)
+    vt = ut.temp2vel(T)
     v = np.linspace(-vt*3, vt*3, num=num)    
     dv = v[1] - v[0]
 
-    omt = ut.temp2vel(300+273) * k
-    om = np.linspace(-omt, omt, num=num)    
+    omt = vt * k
+    om_max = omt
+    om = np.linspace(-om_max, om_max, num=num)    
     dom = om[1] - om[0]
 
     omv_max = omt * 3
+    #omv_max = omt * 20
     omv = np.linspace(om0-omv_max, om0+omv_max, num=num)
     domv = omv[1] - omv[0]
 
     # distributions
     maxboltz = dist.maxwell_boltzmann_1d(v, vt)
-    natlinwidth = dist.natural_linewidth(om, 1/TAU)
-    voigt = dist.gas_dist1d(omv, 0, vp=vt, theta_L=0.0, tau=TAU,
+    natlinwidth = dist.excited_population(om, sat, 1/TAU)
+    voigt = dist.voigt1d(omv, vp=vt, sat=sat, tau=TAU,
                         om0=2*np.pi*PEAK_FREQ, num_om=2**10+1).flatten() # voigt
+    compare_pop = dist.excited_population(omv-om0, sat, 1/TAU)
+    compare_pop = compare_pop * voigt.max() / compare_pop.max()
 
     # integrals of distributions
     int_mb = intg.trapezoid(maxboltz, dx=dv)
-    int_nlw = intg.trapezoid(natlinwidth, dx=dom)
-    int_voi = intg.trapezoid(voigt, dx=domv)
     print(f'Maxwell-Boltzmann scaling speed: {vt} ms⁻¹')
     print('Maxwell-Boltzmann integral: \t' + str(int_mb))
-    print('natural linewidth integral: \t' + str(int_nlw))
-    print('Voigt profile integral: \t' + str(int_voi))
 
     # FWHM checks
+    fwhm_mb, (mb_l, mb_r) = ut.get_FWHM(v, maxboltz)
+    fwhm_nl, (nl_l, nl_r) = ut.get_FWHM(om, natlinwidth)
+    fwhm_vg, (vg_l, vg_r) = ut.get_FWHM(omv, voigt)
+    # theoretical
+    fwhm_mb_t = 2*np.sqrt(np.log(2))*vt
+    fwhm_nl_t = np.sqrt(1+sat)/TAU
+    fwhm_vg_t = fwhm_mb_t * k
+    # rel error
+    er_vg = np.abs((fwhm_vg-fwhm_vg_t)/fwhm_vg_t)
+    er_nl = np.abs((fwhm_nl-fwhm_nl_t)/fwhm_nl_t)
+    er_mb = np.abs((fwhm_mb-fwhm_mb_t)/fwhm_mb_t)
+    print(f'Maxwell-Boltzmann FWHM: \t{fwhm_mb:.5e} ms⁻¹\t\t(theoretical: {fwhm_mb_t:.5e} ms⁻¹, \trel error: {er_mb})')
+    print(f'excited population FWHM: \t{fwhm_nl:.5e} rad·s⁻¹\t\t(theoretical: {fwhm_nl_t:.5e} rad·s⁻¹, \trel error: {er_nl})')
+    print(f'Voigt profile FWHM: \t\t{fwhm_vg:.5e} rad·s⁻¹\t\t(MB theoretical: {fwhm_vg_t:.5e} rad·s⁻¹, \trel error: {er_vg})')
 
     # plots
 
     fig, ax = plt.subplots(1,2)
     ax[0].plot(v/vt, maxboltz)
     ax[0].set_title('Maxwell-Boltzmann')
+    #ax[0].plot([mb_l/vt, mb_l/vt], [0, 0.0012])
+    #ax[0].plot([mb_r/vt, mb_r/vt], [0, 0.0012])
     ax[1].plot(om, natlinwidth)
     ax[1].set_title('Natural linewidth')
+    #ax[1].plot([nl_l, nl_l], [0, 3e-9])
+    #ax[1].plot([nl_r, nl_r], [0, 3e-9])
     ax[0].set_xlabel('v/vp')
     ax[1].set_xlabel('ω - ω₀')
 
     fig, ax = plt.subplots(1,1)
     ax.plot(omv-om0, voigt, label='Voigt')
+    ax.plot(omv-om0, compare_pop, label='static atom')
     ax.set_xlabel('ω - ω₀')
-    ax.set_title('Voigt profile')
-
-    plt.show()
-
-def dist3d():
-    num = 501
-    vt = ut.temp2vel(300+273)
-    v = np.linspace(0, vt*3, num=num)    
-    dv = v[1] - v[0]
-
-    # distributions
-    maxboltz = v**2 * dist.maxwell_boltzmann_3d(v, vt)
-    natlinwidth = 2 * np.abs(ut.get_alphainv(0)) * dist.natural_linewidth(v*ut.get_alphainv(0), 1/TAU)
-
-    # integrals of distributions
-    int_mb = intg.trapezoid(maxboltz, dx=dv)
-    int_nlw = intg.trapezoid(natlinwidth, dx=dv)
-    print(f'Maxwell-Boltzmann scaling speed: {vt} ms⁻¹')
-    print('Maxwell-Boltzmann integral: \t' + str(int_mb))
-    print('natural linewidth integral: \t' + str(int_nlw))
-
-    # plots
-
-    #fig, ax = plt.subplots(1,2)
-    #ax[0].plot(v/vt, maxboltz)
-    #ax[0].set_title('Maxwell-Boltzmann')
-    #ax[1].plot(v/vt, natlinwidth)
-    #ax[1].set_title('Natural linewidth')
-    #ax[0].set_xlabel('v/vp')
-    #ax[1].set_xlabel('v/vp')
-
-    fig, ax = plt.subplots(1,1)
-    ax.plot(v/vt, voigt, label='Voigt')
-    ax.plot(v/vt, maxboltz, label='Maxwell-Boltzmann')
-    #ax.plot(v, natlinwidth, label='Natural linewidth')
-    ax.set_xlabel('v/vp')
     ax.legend()
     ax.set_title('Voigt profile')
 
     plt.show()
 
+def dist3d(voigt_fun):
+    om0 = 2*np.pi*PEAK_FREQ
+    k = om0/cst.speed_of_light
+    T = 1000
+    #T = 1e-5
+    temps = np.linspace(50, T, num=5)
+    angles = np.linspace(0, np.pi/2, num=7)
+    sat = 0.1
+
+    num = 501
+    vt = ut.temp2vel(T)
+    vmax = vt*6
+    v = np.linspace(0, vmax, num=num)    
+    dv = v[1] - v[0]
+
+    omt = vt * k
+    om_max = omt / 5
+    om = np.linspace(-om_max, om_max, num=num)    
+    dom = om[1] - om[0]
+
+    omv_max = omt * 3
+    #omv_max = om0 * 1e-7
+    omv = np.linspace(om0-omv_max, om0+omv_max, num=num)
+    domv = omv[1] - omv[0]
+
+    theta = np.array([0])
+
+    # distributions
+    maxboltz = dist.maxwell_boltzmann_3d(v, vt)
+    beam = dist.beam(v, vt)
+    population = dist.excited_population(om, sat, 1/TAU)
+    voigt = voigt_fun(omv, theta, vp=vt, om0=om0, sat=sat, tau=TAU)
+
+    # for comparison
+    compare_pop = dist.excited_population(omv-om0, sat, 1/TAU)
+    compare_pop = compare_pop * voigt.max() / compare_pop.max()
+    compare_temps = [voigt_fun(omv, theta, vp=ut.temp2vel(T),
+                                      om0=om0, sat=sat, tau=TAU) for T in temps]
+    #compare_temps = [voigt / voigt.max() for voigt in compare_temps]
+    compare_angles = [voigt_fun(omv, theta, vp=vt,
+                                      om0=om0, sat=sat, tau=TAU) for theta in angles]
+
+    # integrals of distributions
+    int_mb = intg.trapezoid(maxboltz, dx=dv)
+    int_bm = intg.trapezoid(beam, dx=dv)
+    print(f'Maxwell-Boltzmann scaling speed: {vt} ms⁻¹')
+    print('gas integral: \t' + str(int_mb))
+    print('beam integral: \t' + str(int_mb))
+
+    # peaks
+    peak_mb = v[maxboltz.argmax()]
+    peak_bm = v[beam.argmax()]
+    # theoretical
+    mb_t = vt
+    bm_t = np.sqrt(3/2) * vt
+    # rel error
+    er_mb = np.abs((peak_mb - mb_t)/mb_t)
+    er_bm = np.abs((peak_bm - bm_t)/bm_t)
+    print(f'gas most probable v: \t{peak_mb:.5e} ms⁻¹\ttheoretical: {mb_t:5e} ms⁻¹\trel error: {er_mb}')
+    print(f'beam most probable v: \t{peak_bm:.5e} ms⁻¹\ttheoretical: {bm_t:5e} ms⁻¹\trel error: {er_bm}')
+
+    # mean
+    mean_mb = np.average(v, weights=maxboltz)
+    mean_bm = np.average(v, weights=beam)
+    # theoretical
+    mb_t = 2 * vt / np.sqrt(np.pi)
+    bm_t = 0.75 * np.sqrt(np.pi) * vt
+    # rel error
+    er_mb = np.abs((mean_mb - mb_t)/mb_t)
+    er_bm = np.abs((mean_bm - bm_t)/bm_t)
+    print(f' gas mean v: \t{mean_mb:.5e} ms⁻¹\ttheoretical: {mb_t:5e} ms⁻¹\trel error: {er_mb}')
+    print(f'beam mean v: \t{mean_bm:.5e} ms⁻¹\ttheoretical: {bm_t:5e} ms⁻¹\trel error: {er_bm}')
+
+    # plots
+
+    fig, ax = plt.subplots(1,2)
+    ax[0].plot(v/vt, maxboltz, label='gas')
+    ax[0].plot(v/vt, beam, label='beam')
+    ax[0].set_title('Maxwell-Boltzmann')
+    ax[0].legend()
+    ax[0].set_xlabel('v/vp')
+    ax[1].plot(om, population)
+    ax[1].set_title('excited population')
+    ax[1].set_xlabel('ω - ω₀')
+
+    fig, ax = plt.subplots(1,1)
+    ax.plot(omv-om0, voigt, label='Voigt')
+    ax.plot(omv-om0, compare_pop, label='static atom')
+    ax.set_xlabel('ω - ω₀')
+    ax.legend()
+    ax.set_title('Voigt profile')
+
+    fig, ax = plt.subplots(1,1)
+    for voigt, T in zip(compare_temps, temps):
+        ax.plot(omv-om0, voigt, label=f'Voigt (T={T} K)')
+    ax.set_xlabel('ω - ω₀')
+    ax.legend()
+    ax.set_title('Voigt profile for different temperatures')
+
+    fig, ax = plt.subplots(1,1)
+    for voigt, angle in zip(compare_angles, angles):
+        ax.plot(omv-om0, voigt, label=f'Voigt ($\\theta$={np.rad2deg(angle)}°)')
+    ax.set_xlabel('ω - ω₀')
+    ax.legend()
+    ax.set_title('Voigt profile for different atom beam-laser angles')
+
+    plt.show()
+
+def dist3d_comp():
+    om0 = 2*np.pi*PEAK_FREQ
+    k = om0/cst.speed_of_light
+    T = 300
+    #T = 1e-5
+    angles = np.linspace(0, 3/4 * np.pi/2, num=3)
+    sat = 0.1
+
+    num = 501
+    vt = ut.temp2vel(T)
+    omt = vt * k
+    om_max = omt * 3
+    om = np.linspace(om0-om_max, om0+om_max, num=num)
+
+    beams = [dist.voigt3d_beam(om, theta, vp=vt,
+                                      om0=om0, sat=sat, tau=TAU) for theta in angles]
+    gases = [dist.voigt3d_gas(om, theta, vp=vt,
+                                      om0=om0, sat=sat, tau=TAU) for theta in angles]
+
+    fig, ax = plt.subplots(1,1)
+    for beam, angle in zip(beams, angles):
+        ax.plot(om-om0, beam, label=f'Beam ($\\theta$={np.rad2deg(angle)}°)')
+    for gas, angle in zip(gases, angles):
+        ax.plot(om-om0, gas, label=f'Gas ($\\theta$={np.rad2deg(angle)}°)')
+    ax.set_xlabel('ω - ω₀')
+    ax.legend()
+    ax.set_title('Voigt profile for different atom beam-laser angles')
+    plt.show()
+
 if __name__ == '__main__':
     dist1d()
-    #dist3d()
+    #dist3d(dist.voigt3d_gas)
+    #dist3d_comp()
 
