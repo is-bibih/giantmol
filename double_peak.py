41c7ea5ba82a8c8098f1b62139de450283d31805
vivi <vivianamaldes@gmail.com>
2025-03-12 14:43:25 +0100

    nicer analysis for double peak

--- double_peak.py
+++ double_peak.py
@@ -1,11 +1,11 @@
 from os import listdir, path
-from re import X
 
 import utilities as ut
 import constants as cst
 
 import numpy as np
 import scipy.signal as sgl
+from scipy.constants import speed_of_light
 import matplotlib.pyplot as plt
 
 data_dir = './data/20250213_Ca-neutral_double-peak'
@@ -18,7 +18,7 @@ counts = [dat[0] for dat in data]
 freqs = [dat[1] for dat in data]
 temps = ut.cel2kel(np.array([ut.fname2temp(f) for f in fnames])) # convert to kelvin
 
-# reorder by temperature
+# sort by temperature
 idx = np.argsort(temps)
 counts = [counts[i] for i in idx]
 freqs = [freqs[i] for i in idx]
@@ -43,16 +43,30 @@ mean_trans_freq = valley_freqs.mean()
 # distance between peaks
 
 peak_distances = np.zeros((N,))
-all_peak_idx = []
+peak_idx = np.zeros((N,2), dtype=int) 
+
 for i in range(N):
-    c = counts[i]
-    all_peak_idx.append(sgl.find_peaks_cwt(c, np.arange(1,400)))
-    all_peak_idx[i] = all_peak_idx[i][all_peak_idx[i] > 100]
-    print(all_peak_idx[i])
-    highest_idx = np.argpartition(c[all_peak_idx[i]], -2)[-2:]
-    peak_idx = all_peak_idx[i][highest_idx]
-    print('results')
-    print(peak_idx)
+    threshold = 0.5
+    c = (counts[i]/counts[i].max()).flatten() # normalize
+    c = sgl.savgol_filter(c, window_length=int(c.size * 0.07), polyorder=3) # smooth
+    # remove lower values for easier peak search
+    high_values = c > threshold
+    first_high = high_values.nonzero()[0][0]
+    first_low = np.nonzero(np.logical_not(high_values[first_high:]))[0][0] + first_high
+    high_values[first_low:] = np.zeros((len(high_values)-first_low))
+    c = c * high_values
+    # find all peaks (there's a lot from the noise)
+    all_peak_idx, props = sgl.find_peaks(c, height=(None, None), width=100)
+    peak_heights = props['peak_heights']
+    # choose the highest two
+    highest_idx = np.argpartition(peak_heights, -2)[-2:]
+    # store
+    this_peak_idx = all_peak_idx[highest_idx]
+    peak_idx[i,:] = this_peak_idx
+    # calculate distances
+    peak_distances[i] = np.abs(freqs[i][this_peak_idx[1]] - freqs[i][this_peak_idx[0]])
+
+peak_speeds = 0.5 * peak_distances * (speed_of_light/cst.TRANSITION_FREQ)
 
 # ------------------------------------------------------------------------------------------
 
@@ -72,15 +86,25 @@ fig, ax = plt.subplots(1,1)
 for i in range(N):
     ax.scatter(freqs[i] * xaxis_scale, counts[i], label=f'{temps[i]:.2f} K', marker='.')
     #ax.plot(np.arange(len(counts[i])), counts[i], label=f'{temps[i]:.2f} K')
-for i in range(N): ax.scatter(freqs[i][all_peak_idx[i]] * xaxis_scale, counts[i][all_peak_idx[i]], marker='*', c='red', s=80)
+for i in range(N): ax.scatter(freqs[i][peak_idx[i]] * xaxis_scale, counts[i][peak_idx[i]], marker='*', c='red', s=80)
 ax.set_xlabel('frequency (GHz)')
 ax.set_ylabel('counts / ms')
 fig.legend()
 
 fig, ax = plt.subplots(1,1)
 ax.scatter(temps, valley_freqs * xaxis_scale)
-ax.set_xlabel('temperature (K)')
-ax.set_ylabel('valley frequency (GHz)')
+ax.set_xlabel('oven temperature (K)')
+ax.set_ylabel('transition frequency (GHz)')
+
+fig, ax = plt.subplots(1,1)
+ax.scatter(temps, peak_speeds, label='experimental data')
+ax.plot(temps, ut.temp2vel(temps) * np.cos(np.deg2rad(75)),
+        label='speed from oven temp,\n$\\theta = 75\\degree$')
+ax.plot(temps, ut.temp2vel(temps*0.2) * np.cos(np.deg2rad(75)),
+        label='speed from oven temp · 0.2,\n$\\theta = 75\\degree$')
+ax.set_xlabel('oven temperature (K)')
+ax.set_ylabel(r'$v_p$cos$(\theta_{OL})$ most probable speed (ms⁻¹)')
+ax.legend()
 
 plt.show()
 
