b8fd6e9619de07200cc4ee6de549e433cfee9851
vivi <vivianamaldes@gmail.com>
2025-03-12 10:59:42 +0100

    script for transforming raw data to counts/ms and freq

--- utilities.py
+++ utilities.py
@@ -11,13 +11,18 @@ get_alpha = lambda th, om0=PEAK_FREQ*2*np.pi, theta_L=0: - cst.c/(om0*np.cos(th 
 get_alphainv = lambda th, om0=PEAK_FREQ*2*np.pi, theta_L=0: - om0*np.cos(th - theta_L)/cst.c
 temp2vel = lambda T, m=CA40_MASS: np.sqrt(2*cst.Boltzmann*T/m)
 vel2temp = lambda v, m=CA40_MASS: m*v**2 / (2*cst.Boltzmann)
+cel2kel = lambda c: c+273.15
+kel2cel = lambda k: k-273.15
 freq2om = lambda f: 2*np.pi*f
 om2freq = lambda om: 0.5*om/np.pi
+
+fname2temp = lambda fname: float(f'{m.group(1)}.{m.group(2)}') \
+    if (m := re.match(r"(\d+)-(\d)C", fname)) else None
 
 TIME_STEP_FILENAME = 'timestep.txt'
 VOLTAGE_FREQUENCY_FILENAME = 'voltage_frequency.dat'
 
-def read_data(data_dir):
+def read_raw_data(data_dir):
     fnames = [f for f in listdir(data_dir) \
               if path.isfile(path.join(data_dir, f)) \
               and not (f == TIME_STEP_FILENAME or f == VOLTAGE_FREQUENCY_FILENAME)]
@@ -34,8 +39,7 @@ def read_data(data_dir):
     voltages = [d[:,4] for d in data] 
     counts = [d[:,1] / timestep for d in data] 
     freqs = [np.array(get_freq(V)) for V in voltages]
-    temps = [float(f'{m.group(1)}.{m.group(2)}') for t in fnames \
-             if (m := re.match(r"(\d+)-(\d)C", t))]
+    temps = [fname2temp(t) for t in fnames]
 
     return {
         'voltages': voltages, 
@@ -45,6 +49,12 @@ def read_data(data_dir):
         'temperatures': temps,
         'filenames': fnames,
     }
+
+def read_counts_freq(fname):
+    data = np.loadtxt(fname)
+    freq = data[:,0]
+    counts = data[:,1]
+    return counts, freq
 
 def clean(ct):
     """normalize and set background to zero"""
@@ -58,8 +68,8 @@ def remove_small(ct, threshold, *other_quantities):
     ct = ct[idx]
     return ct, *[qty[idx] for qty in other_quantities]
 
-def read_preprocess(data_dir, ref_V, ref_f, threshold=0.0):
-    V, f, ct = read_data(data_dir, ref_V, ref_f)
+def read_preprocess(data_dir, threshold=0.0):
+    V, f, ct = read_raw_data(data_dir)
     ct = [clean(c) for c in ct]
     if threshold:
         for i in range(len(ct)):
